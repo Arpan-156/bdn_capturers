edd28b8bee0f2141dd08c63a107781d1b55d7b57
unknown <arpanganguly30@gmail.com>
2022-06-12 21:29:14 +0530

    ADDED testimonials & some others

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,14 @@
+
+  
+// Facts counter
+$('[data-toggle="counter-up"]').counterUp({
+  delay: 20,
+  time: 20
+});
+
+
+
+
 "use strict";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
@@ -378,6 +389,8 @@ docReady(navbarInit);
 docReady(detectorInit);
 docReady(scrollToTop);
 //# sourceMappingURL=theme.js.map
+
+
 
 
 
